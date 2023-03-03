617d8b3dda5b1a74b204852f92ab03d89829efb5
Adrien Peiffer <adrien.peiffer@acsone.eu>
2023-03-03 16:00:15 +0100

    Add auth_oauth_ropc

---
+++ auth_oauth_ropc/models/res_users.py
@@ -0,0 +1,24 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+from odoo.exceptions import AccessDenied
+
+
+class ResUsers(models.Model):
+
+    _inherit = "res.users"
+
+    def _check_credentials(self, password, env):
+        try:
+            return super(ResUsers, self)._check_credentials(password, env)
+        except AccessDenied:
+            passwd_allowed = (
+                env["interactive"] or not self.env.user._rpc_api_keys_only()
+            )
+            if passwd_allowed and self.env.user.active:
+                ropc_providers = self.env["oauth.ropc.provider"].sudo().search([])
+                for conf in ropc_providers:
+                    if conf._authenticate(self.env.user.login, password):
+                        return
+            raise
