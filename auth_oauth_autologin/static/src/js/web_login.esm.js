1af745f745c501403d6fa4c436b95cb2e643921f
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2026-01-06 16:32:17 +0100

    [FIX] auth_oauth_autologin: compatibility with auth_totp
    The auth_totp URL is /web/login/totp. Before this commit it erroneously triggered the autologin redirect.

--- auth_oauth_autologin/static/src/js/web_login.esm.js
+++ auth_oauth_autologin/static/src/js/web_login.esm.js
@@ -12,7 +12,8 @@ publicWidget.registry.login.include({
     start: async function () {
         const def = this._super.apply(this, arguments);
         let url = window.location.href;
-        if (url.includes("/web/login")) {
+        const parsed_url = new URL(url);
+        if (parsed_url.pathname === "/web/login") {
             url = url.replace("/web/login", "/web");
         }
         this._result = await this._rpc("/auth/auto_login_redirect_link", {
